47dec3c374fc16617fec44f938fa15a8b3028042
lichao306153702 <306153702@qq.com>
2017-12-03 15:09:06 +0800

    first

---
+++ src/main/java/com/example/demo/SsmeasyuiuApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmeasyuiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmeasyuiuApplication.class, args);
+	}
+}
